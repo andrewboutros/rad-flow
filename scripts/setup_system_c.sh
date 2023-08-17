b67483a4d687c739fec9eeacd89e2639c80a3e7f
George Trieu <trieugeorge@gmail.com>
2023-08-17 16:22:43 -0400

    Added CI for RAD-Sim on GitHub Workflows

---
+++ scripts/setup_system_c.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update && apt-get upgrade -y
+sudo apt install build-essential cmake
+
+# SYSTEMC
+
+# Set variables for SystemC
+SYSTEMC_VERSION="2.3.4"
+
+wget -O systemc-$SYSTEMC_VERSION.tar.gz https://github.com/accellera-official/systemc/archive/refs/tags/$SYSTEMC_VERSION.tar.gz
+tar xzf systemc-$SYSTEMC_VERSION.tar.gz
+rm -f systemc-$SYSTEMC_VERSION.tar.gz
+cd systemc-$SYSTEMC_VERSION
+mkdir build
+cd build
+cmake ../ -DCMAKE_CXX_STANDARD=11
+cmake --build .
