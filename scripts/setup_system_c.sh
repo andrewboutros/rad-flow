179c3d62b13a5d0faef8be4a39674cf29b9ad31f
George Trieu <trieugeorge@gmail.com>
2023-10-16 22:36:44 -0400

    Caching for SystemC build files

--- scripts/setup_system_c.sh
+++ scripts/setup_system_c.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
+usage()
+{
+   echo ""
+   echo "Usage: $0 -v version"
+   echo -e "\t-v The SystemC version to setup (default: 2.3.4)"
+}
+
+while getopts "v:h" opt
+do
+   case "$opt" in
+      v) SYSTEMC_VERSION="$OPTARG" ;;
+      h) usage
+         exit 0
+         ;;
+      *) usage
+         exit 1
+         ;;
+   esac
+done
+
+if [ -z "${SYSTEMC_VERSION}" ]; then
+    usage
+    exit 1
+fi
 
 sudo apt-get update && apt-get upgrade -y
 sudo apt install build-essential cmake
-
-# SYSTEMC
-
-# Set variables for SystemC
-SYSTEMC_VERSION="2.3.4"
 
 wget -O systemc-$SYSTEMC_VERSION.tar.gz https://github.com/accellera-official/systemc/archive/refs/tags/$SYSTEMC_VERSION.tar.gz
 tar xzf systemc-$SYSTEMC_VERSION.tar.gz
