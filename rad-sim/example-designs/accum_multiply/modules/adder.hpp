c8d8a6c6231f8ceb31beb520915a793e795fa663
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-05-13 16:06:29 -0400

    added base code for adder.hpp

--- rad-sim/example-designs/accum_multiply/modules/adder.hpp
+++ rad-sim/example-designs/accum_multiply/modules/adder.hpp
@@ -2,7 +2,7 @@
 
 #include <aximm_interface.hpp>
 #include <design_context.hpp>
-#include <queue>
+#include <fifo.hpp>
 #include <radsim_defines.hpp>
 #include <radsim_module.hpp>
 #include <string>
@@ -11,34 +11,52 @@
 
 #define DATAW 512
 
-class requester : public RADSimModule {
+// This code is based on the requester from the aximm_hello_world example
+
+// This module receives data from the driver, and sums values in groups of 4
+// Then the data outputs the sum via ofifo
+// Only data in groups of 4 will be summed, so the 3rd data is not summed with the 5th data
+// Data is summed like: (1,2,3,4) (5,6,7,8)
+// Numbers are input into a shift register before summed, sum is only recorded in period of 4
+
+class adder : public RADSimModule {
 private:
-  std::queue<uint64_t> req_addr_fifo;      // FIFO to store request addresses
-  std::queue<sc_bv<DATAW>> req_wdata_fifo; // FIFO to store request data
-  std::queue<bool> req_type_fifo; // FIFO to store request type (0: R, 1: W)
-  unsigned int req_fifo_depth;    // Depth of request FIFOs
-  sc_signal<bool> req_fifo_full;  // Signal flagging request FIFO is full
-  sc_signal<bool> aw_accepted; // Signal flagging that AW transaction accepted
-  sc_signal<unsigned int> id_counter;  // Counter for transaction IDs
-  unsigned int num_received_responses; // Coutnter for received responses
+  // Modules and Signals Only (supposed to be) Internally Accessible
+  fifo<int16_t> *ififo; // Input FIFO
+  sc_int<16> internal_registers[4]; // Storing 4 numbers that is to be summed and outputted
+  sc_signal<bool> sum_valid; // Signal flagging if all 4 numbers are ready (triggered by an internal counter)
+  fifo<int16_t> *ofifo; // Output FIFO (here we ignore edge case of overflow from adding 4 numbers)
+
+  // Internal Variables
+  unsigned int num_values_received;
+
+  // TODO
+  // To write to ofifo:
+  // ofifo.read_signal.write(data_vector<int16_t>)
+
 
 public:
-  sc_in<bool> rst;
-  // Interface to driver logic
-  sc_in<uint64_t> req_addr;
-  sc_in<bool> req_type;
-  sc_in<sc_bv<DATAW>> req_wdata;
-  sc_in<bool> req_valid;
-  sc_out<bool> req_ready;
-  sc_out<unsigned int> received_responses;
-  // Interface to the NoC
-  aximm_master_port aximm_req_interface;
+  // Signal accessible by external modules
+  sc_in<bool> rst; // Reset Signal
 
-  requester(const sc_module_name &name, unsigned int fifo_depth);
-  ~requester();
+  // Input side signals
+  sc_in<sc_int<16>> input;
+  sc_in<bool> input_valid;
+  sc_out<bool> input_ready;
+
+  // Output side signals
+  sc_out<sc_int<16>> output;
+  sc_in<bool> output_ready;
+  sc_out<bool> output_valid;
+
+  // Interface to the NoC
+  axis_master_port axis_adder_interface;
+
+  adder(const sc_module_name &name, unsigned int fifo_depth);
+  ~adder();
 
   void Assign(); // Combinational logic process
   void Tick();   // Sequential logic process
-  SC_HAS_PROCESS(requester);
+  SC_HAS_PROCESS(adder);
   void RegisterModuleInfo();
 };
