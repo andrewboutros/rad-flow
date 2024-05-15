5f679234fad1dfdee342fb039482f55e6d58bfa5
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-05-15 11:40:49 -0400

    need input temp data vector for axis input for multiplier.hpp

--- rad-sim/example-designs/accum_multiply/modules/multiplier.hpp
+++ rad-sim/example-designs/accum_multiply/modules/multiplier.hpp
@@ -37,6 +37,7 @@ private:
   // Internal Variables
   unsigned int ififo_depth; // Number of values that can be stored in ififo, assigned in constructor
   unsigned int ofifo_depth; // Number of values that can be stored in ofifo, assigned in constructor
+  data_vector<int16_t> input_data_temp; // Temp data vector to convert values to data_vector
   data_vector<int16_t> output_data_temp; // Temp data vector to convert values to data_vector
 
 public:
