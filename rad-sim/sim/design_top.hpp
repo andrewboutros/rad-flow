8734eacff42313fd622213923c122af3d786348a
abnashkb <abnashkb@gmail.com>
2024-09-24 02:09:04 -0400

    Fixed comment bug

--- rad-sim/sim/design_top.hpp
+++ rad-sim/sim/design_top.hpp
@@ -20,7 +20,7 @@ class RADSimDesignTop : virtual public sc_module {
             std::strcpy(module_name, module_name_str.c_str());
             portal_inst = new portal(module_name, radsim_design);
 
-            connect master to master instead, to expose to top
+            //connect master to master instead, to expose to top
             portal_inst->portal_axis_master.ConnectToPort(this->design_top_portal_axis_master);
             portal_inst->portal_axis_slave.ConnectToPort(this->design_top_portal_axis_slave); //top drives portal bc top receives slave inputs 
             #endif
